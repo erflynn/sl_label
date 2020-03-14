30eeb892d5de5a14de700bc3a466b86191f67d73
erflynn <erflynn@stanford.edu>
2020-03-14 10:58:46 -0700

    updated the rnaseq extraction scripts

--- code/00_download_human.sh
+++ code/00_download_human.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=download
-#SBATCH --output=logs/download.out
-#SBATCH --error=logs/download.err
+#SBATCH --output=download.out
+#SBATCH --error=download.err
 #SBATCH --time=6:00:00 
 #SBATCH --mem=15000
 #SBATCH --partition=rbaltman
@@ -10,6 +10,6 @@
 
 #wget https://data-refinery-s3-compendia-circleci-prod.s3.amazonaws.com/kfc8oetnjca248dx4lpr805a_ac0d90a1-25c0-4617-838f-212bede63702_compendia.zip
 
-#cd humans/
-#unzip human_compendia.zip
-wget https://data-refinery-s3-compendia-circleci-prod.s3.amazonaws.com/HOMO_SAPIENS_1_1574170428.zip
+#wget https://data-refinery-s3-compendia-circleci-prod.s3.amazonaws.com/HOMO_SAPIENS_1_1574170428.zip
+
+unzip HOMO_SAPIENS_1_1574170428.zip
--- code/02e_grab_rnaseq.R
+++ code/02e_grab_rnaseq.R
@@ -7,7 +7,7 @@ options(stringsAsFactors = FALSE)
 
 args <- commandArgs(trailingOnly=TRUE)
 prefix <- args[1]
-idx <- args[2]
+idx <- as.numeric(args[2])
 
 extractChunk <- function(my.list, idx, SIZE.CHUNK=50){
   NUM.CHUNKS <- ceiling(length(my.list)/SIZE.CHUNK)
@@ -17,35 +17,62 @@ extractChunk <- function(my.list, idx, SIZE.CHUNK=50){
 
 
 studyMat <- function(study_id){
-  sample_list <- mapping2 %>% filter(study_acc==study_id)
+print(study_id)
+   out.path <- sprintf("data/rnaseq/%s/01_study_mat/%s.csv", prefix, study_id)
+   if (file.exists(out.path) &  file.info(out.path)$size!=0){
+       print("file already generated")
+       return(NA)
+  }
+
+  sample_list <- mapping %>% filter(study_acc==study_id & !is.na(sample_acc) & !is.na(study_acc))
   sample_dat <- lapply(sample_list$sample_acc, function(sample_id){
     sample.path <- sprintf("data/rnaseq/%s/%s/%s_quant.sf", 
                            prefix, study_id, sample_id);
     print(sample_id);
     if (file.exists(sample.path)){
-      quant_sf <- read_tsv(sample.path) %>% dplyr::select(Name, TPM);
+      my_dat <- read_tsv(sample.path) 
+      if (!"Name" %in% colnames(my_dat)){
+      	 print(sprintf("corrupted file %s", sample.path))
+	 return(NA)
+      	 #my_dat <- read.delim(sample.path, skip=1)
+	 #colnames(my_dat) <- c("Name", "Length","EffectiveLength","TPM", "NumReads")
+      }
+      quant_sf <- my_dat %>% dplyr::select(Name, TPM);
       colnames(quant_sf) <- c("gene_name", sample_id);
       
-      if (sample_id==sample_list$sample_acc[[1]]){
-        return(quant_sf)
-      }
+      #if (sample_id==sample_list$sample_acc[[1]]){
+      #  return(quant_sf)
+      #}
       
-      return(quant_sf[,2])
+      return(quant_sf)
     } else {
       return(NA)
     }
   })
-  
-  study_df <- data.frame(do.call(cbind, sample_dat[!is.na(sample_dat)]))
+  sample_dat2 <- sample_dat[!is.na(sample_dat)]
+  if (all(is.na(sample_dat))){
+     return(NA)
+  }
+  row_lengths <- sapply(sample_dat2, nrow)
+  if (all(row_lengths==row_lengths[1])){
+     study_d1 <- data.frame(do.call(cbind, lapply(sample_dat2, function(df) df %>% select(-gene_name))))
+     study_df <- cbind("gene_name"=sample_dat2[[1]]$gene_name, study_d1)
+  } else {
+    print(sprintf("Row lengths do not match for %s: %s", prefix, study_id))
+    study_df <- sample_dat2 %>% reduce(full_join, by="gene_name")
+  }
   study_df %>% 
-    write_csv(sprintf("data/rnaseq/%s/01_study_mat/%s.csv", prefix, study_id))
+    write_csv(out.path)
 }
 
 
 # read in the mapping file
 mapping <- fread(sprintf("data/%s/02_sample_lists/%s_rnaseq_exp_to_sample.csv", prefix, prefix), 
-                 data.table=FALSE)
-list.studies <- unique(mapping$study_acc)
-small.list <- extractChunk(list.studies, idx, 100)
+                 data.table=FALSE) %>% filter(!is.na(study_acc))
+#list.studies <- unique(mapping$study_acc)
+list.studies <- list.files(sprintf("data/rnaseq/%s/", prefix), pattern="*RP*")
+print(length(list.studies))
+small.list <- extractChunk(list.studies, idx, SIZE.CHUNK=500)
+print(small.list)
 lapply(small.list, studyMat)
 
