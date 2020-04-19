4e3139b5e3bbec7c150c45b7fbe7e5a1be29fb5c
E Flynn <erflynn@stanford.edu>
2020-04-19 16:11:50 -0700

    cleaned up directories, renumbered files

--- code/03_models/04_general_cv_grp.R
+++ code/03_models/04_general_cv_grp.R
@@ -2,50 +2,79 @@ require('data.table')
 require('tidyverse')
 require('groupdata2')
 require('glmnet')
+require('bestNormalize')
 #install.packages("~/Downloads/glmnet_3.0-2.tgz", repos=NULL)
 # **NEED GLMNET3.0**
 
 source("code/03_models/cv_utils.R")
 
 options(stringsAsFactors = FALSE)
-NFOLDS <- 8
+NFOLDS <- 6
 prefix <- "human"
 data_type <- "microarray"
-ds <- "cell"
+ds <- "sex"
 args <- commandArgs(trailingOnly=TRUE)
 prefix <- args[1]
 data_type <- args[2]
 ds <- args[3]
+idx <- as.numeric(args[4])
 
 # set up the data
-pos <- fread(sprintf("data/%s/%s/04_sl_input/%s_%s_pos_expr.csv", data_type, prefix, prefix, ds ), data.table=FALSE)
-neg <- fread(sprintf("data/%s/%s/04_sl_input/%s_%s_neg_expr.csv", data_type, prefix, prefix, ds ), data.table=FALSE)
 
 if (data_type == "microarray"){
   #mapping <- fread(sprintf("data/01_metadata/%s_exp_to_sample.csv", prefix), data.table=FALSE)  
   mapping <- fread(sprintf("data/01_metadata/%s_compendia_dedup_mapping.csv", prefix, data_type), data.table = FALSE) %>%
     rename(study_acc=study_str)
+  pos <- fread(sprintf("data/05_train_df/%s_%s_%s_pos_expr.csv",prefix, data_type, ds ), data.table=FALSE)
+  neg <- fread(sprintf("data/05_train_df/%s_%s_%s_neg_expr.csv", prefix, data_type, ds ), data.table=FALSE)
+
 } else {
   MIN.READS <- 100000
   # RNA-seq data does not have duplicates... interesting
   mapping <- fread(sprintf("data/01_metadata/%s_exp_to_sample_counts.csv", prefix), data.table=FALSE)  %>%
     filter(present & num_reads >= MIN.READS)
+  miceadds::load.Rdata(sprintf("data/05_train_df/%s_rnaseq_%s_pos_expr.RData", prefix, ds), "pos")
+  miceadds::load.Rdata(sprintf("data/05_train_df/%s_rnaseq_%s_neg_expr.RData", prefix, ds), "neg")
+  pos <- pos %>% rename(rid=gene_name)
+  neg <- neg %>% rename(rid=gene_name)
 }
 
 
 
 pos_map <- mapping %>% filter(sample_acc %in% colnames(pos))
 neg_map <- mapping %>% filter(sample_acc %in% colnames(neg))
+
+# deal with class imbalance
+set.seed(304)
+num_pos <- nrow(pos_map)
+num_neg <- nrow(neg_map)
+
+if ((num_pos > (1.2*num_neg)) | (num_neg > (1.2*num_pos))){
+  num_ea <- min(num_neg, num_pos)
+  pos_map <- pos_map %>% sample_n(num_ea)
+  neg_map <- neg_map %>% sample_n(num_ea)
+  
+}
+
 my_dat <- bind_rows(pos_map %>% mutate(class=1), 
                     neg_map %>% mutate(class=0)) %>%
   mutate(class=factor(class), study_acc=factor(study_acc))
 
 # training + testing division
-set.seed(304)
-train_test_split <- partition_group_data(my_dat, grp_col="study_acc", nfolds=5)
+if (nrow(my_dat) < 1000){
+  tfolds <- 4
+  NFOLDS <- 4
+} else {
+  tfolds <- 5
+}
+
+train_test_split <- partition_group_data(my_dat, grp_col="study_acc", nfolds=tfolds)
 train_test_split %>% group_by(partition, class) %>% count()
-train_part <- train_test_split %>% filter(partition!=5) %>% arrange(class, sample_acc)
-test_part <- train_test_split %>% filter(partition==5) %>% arrange(class, sample_acc)
+train_part <- train_test_split %>% filter(partition!=tfolds) %>% arrange(class, sample_acc)
+test_part <- train_test_split %>% filter(partition==tfolds) %>% arrange(class, sample_acc)
+
+
+
 
 Y_train <- train_part$class
 Y_test <- test_part$class
@@ -63,20 +92,46 @@ colnames(X_test) <- neg$rid
 rownames(X_train) <- c(train_part$sample_acc)
 rownames(X_test) <- c(test_part$sample_acc)
 
+if (ds=="sex"){
+
+   if (data_type=="rnaseq"){
+      xy_genes <- read_csv(sprintf("data/%s/%s/03_model_in/xy_genes_%s.csv", data_type,prefix, data_type))
+      xy_genes_l <- xy_genes$transcript
+   } else {
+      xy_genes <- read_csv(sprintf("data/%s/%s/04_sl_input/xy_genes.csv", data_type,prefix))
+     xy_genes_l <- xy_genes$overlap.genes
+   }
+   X_test <- X_test[,xy_genes_l]
+   X_train <- X_train[,xy_genes_l]
+}
+
+
+
 
 # --- preprocess as needed (e.g. boxcox for RNASeq) --- #
 if (data_type=="rnaseq"){
+  X_train[is.na(X_train)] <- 0
+  num_zeros <- apply(X_train, 2, function(x) sum(x==0)) # remove > 70% zeros
+  X_train <- X_train[,(num_zeros/nrow(X_train) <= 0.7)]
+  
+  list_genes <- colnames(X_train)
+  X_test <- X_test[,list_genes]
   
   X_train <- apply(X_train, 2, function(col) boxcox(col+0.5)$x.t) 
   X_test <- apply(X_test, 2, function(col) boxcox(col+0.5)$x.t)
+ 
   
 }
 
+save(X_train, X_test, Y_train, Y_test, 
+     file=sprintf("data/07_model_dat/%s_%s_%s_train_mat.RData", prefix, data_type, ds))
+
+print("data set up")
 # CV folds
-rand_genes <- sample(1:ncol(X_test), 2000)
+#rand_genes <- sample(1:ncol(X_test), 2000)
 
 
-run_fold <- function(my.fold, nfolds=NFOLDS){
+run_fold <- function(my.fold, nfolds){
   train_data <- fold_list %>% filter(!partition %in% my.fold)
   valid_data <- fold_list %>% filter(partition %in% my.fold)
   # redo the training fold
@@ -89,6 +144,19 @@ run_fold <- function(my.fold, nfolds=NFOLDS){
   X_valid <- X_train[valid_data$sample_acc,]
   Y_valid <- valid_data$class
   
+  # if (prefix == "rat")
+  # design <- as.matrix(as.numeric(as.character(Y_train2)))
+  # fit <- lmFit(t(X_train2), design)
+  # fit <- eBayes(fit)
+  # tt <-topTable(fit, number=ncol(X_train2))
+  # tt$transcript <- rownames(tt)
+  # top_f <- tt %>% arrange(logFC) %>% head(30)
+  # top_m <- tt %>% arrange(desc(logFC)) %>% head(30)
+  # 
+  # list_transcripts <- c(top_f$transcript, top_m$transcript)
+  # 
+  
+  
   if (data_type=="microarray"){
     standardizeFlag = TRUE
   } else {
@@ -98,7 +166,7 @@ run_fold <- function(my.fold, nfolds=NFOLDS){
   test_params <- function(my.alpha, adaptive, relax){
     # add adaptive lasso
     if (adaptive){
-      ridge_cv = cv.glmnet(X_train2[,rand_genes], Y_train2, 
+      ridge_cv = cv.glmnet(X_train2, Y_train2, 
                         family="binomial",
                         foldid=train_folds,
                         alpha=0, 
@@ -107,7 +175,7 @@ run_fold <- function(my.fold, nfolds=NFOLDS){
       best_ridge_coef <- coef(ridge_cv, s = ridge_cv$lambda.min)
       best_ridge_coef <- as.numeric(best_ridge_coef)[-1]
       
-      cvfit = cv.glmnet(X_train2[,rand_genes], Y_train2, 
+      cvfit = cv.glmnet(X_train2, Y_train2, 
                         family="binomial",
                         foldid=train_folds,
                         alpha=my.alpha, 
@@ -115,17 +183,17 @@ run_fold <- function(my.fold, nfolds=NFOLDS){
                         trace=TRUE,
                         penalty.factor = 1 / abs(best_ridge_coef))
     } else {
-      cvfit = cv.glmnet(X_train2[,rand_genes], Y_train2, 
+      cvfit = cv.glmnet(X_train2, Y_train2, 
                         family="binomial",
                         foldid=train_folds,
                         alpha=my.alpha, 
                         standardize=standardizeFlag,
-                        trace=TRUE)
+                        trace=FALSE)
     }
     
-    train_assess <- assess.glmnet(cvfit, newx=X_train2[,rand_genes], newy=Y_train2)
-    valid_pred <- predict(cvfit, newx=X_valid[,rand_genes], type="response")
-    valid_assess <- assess.glmnet(cvfit, newx=X_valid[,rand_genes], newy=Y_valid)
+    train_assess <- assess.glmnet(cvfit, newx=X_train2, newy=Y_train2)
+    valid_pred <- predict(cvfit, newx=X_valid, type="response")
+    valid_assess <- assess.glmnet(cvfit, newx=X_valid, newy=Y_valid)
     my.lambda <- cvfit$lambda.1se
     
     ta <- unlist(train_assess) 
@@ -147,31 +215,34 @@ run_fold <- function(my.fold, nfolds=NFOLDS){
     return(list("tv"=train_valid_assess, "ydf"=df))
   }
   
-  
-  param_res <- data.frame()
-  for (my.alpha in seq(0,1, 0.1)){
-    alpha_res <- test_params(my.alpha)
-    if (my.alpha==0){
-      param_res <- alpha_res
-    } else {
-      param_res <- rbind(param_res, alpha_res)
-    }
-  }
-  return(param_res)
+
+  res1 <- lapply(seq(0,1,0.1), function(my.alpha) test_params(my.alpha, FALSE) )
+  #res2 <- lapply(seq(0,1,0.1), function(my.alpha) test_params(my.alpha, TRUE) )
+  return(c(res1))
 }
 
-fold_res <- data.frame()
-folds <- unique(train_valid$fold)
-for (i in 1:length(folds)){
-  print(i)
-  res <- run_fold(folds[i])
-  if (i==1){
-    fold_res <- res
-  }
-  else {
-    fold_res <- rbind(fold_res, res)
-  }
-} 
+#lapply(1:10, function(it){
+#  fold_list <- partition_group_data(train_part %>% select(-partition), nfolds=NFOLDS) %>% arrange(class, sample_acc)
+#  samp_to_fold <- fold_list$partition
+#  names(samp_to_fold) <- fold_list$sample_acc
+#  lapply(1:4, function(idx) {
+    res <- run_fold(idx, NFOLDS);
+    save(res, file=sprintf("data/06_fold_dat/fold_%s_%s_%s_%s.RData", prefix,data_type, ds, idx))
+#  })
+#})
+       
+# fold_res <- data.frame()
+# folds <- unique(train_valid$fold)
+# for (i in 1:length(folds)){
+#   print(i)
+#   res <- run_fold(folds[i])
+#   if (i==1){
+#     fold_res <- res
+#   }
+#   else {
+#     fold_res <- rbind(fold_res, res)
+#   }
+# } 
 
 
 
--- code/04_apply_models/01_get_folds.R
+++ code/04_apply_models/01_get_folds.R
@@ -1,16 +1,43 @@
-
-prefix <- "mouse"
-data_type <- "rnaseq"
-ds <- "sex"
-
-
+# look at the fold output from training models
 require('tidyverse')
-full_df <- do.call(rbind, lapply(3:6, function(idx){
+require('glmnet')
+args <- commandArgs(trailingOnly = TRUE)
+prefix <- args[1]
+data_type <- args[2]
+ds <- args[3]
+
+# my_l <- list.files("data/06_fold_dat", pattern="fold_rat_microarray")
+# 
+# full_df <- do.call(rbind, lapply(my_l, function(my_f){
+#   load(file=sprintf("data/06_fold_dat/%s", my_f))
+#   df <- do.call(rbind, lapply(res, function(x) x$tv))
+#   df$fold <- idx
+#   return(df)
+# }))
+
+full_df <- do.call(rbind, lapply(1:4, function(idx){
   load(sprintf("data/06_fold_dat/fold_%s_%s_%s_%s.RData", prefix, data_type, ds, idx))
   df <- do.call(rbind, lapply(res, function(x) x$tv))
   df$fold <- idx
   return(df)
 }))
+
+
+# my_f <- sapply(1:8, function(it) {
+#   lapply(1:4, function(idx){
+#   my.f <- (sprintf("data/06_fold_dat/fold_%s_%s_%s_%s-it%s.RData", prefix, data_type, ds, idx, it))
+#   if (file.exists(my.f)){
+#     load(my.f)
+#   } else {
+#     return(NA)
+#   }
+#   df <- do.call(rbind, lapply(res, function(x) x$tv))
+#   df$fold <- idx
+#   df$it <- it
+#   return(df)})
+# })
+# 
+# my_df <- do.call(rbind, my_f[!is.na(my_f)])
 
 full_df %>% write_csv(sprintf("data/06_fold_dat/%s_%s_%s_combined_params.csv", prefix, data_type, ds))
 
@@ -83,114 +110,31 @@ all_ydf <- do.call(rbind, lapply(1:6, function(idx){
   return(res[[new_idx]]$ydf)
 }))
 
-ydf2 <- all_ydf %>% mutate(ypred=round(as.numeric(yhat))) 
+ydf2 <- all_ydf %>% mutate(ypred=round(as.numeric(as.character(yhat))) )
 
 table(ydf2[,c("y","ypred")])
 
 cv_accuracy <- sum(ydf2$y==ydf2$ypred)/nrow(all_ydf) 
 # mouse microarray (all genes) 0.922 --> 0.920
 # human microarray 0.912
-# rat 0.814
+# rat 0.814 m --> 0.737
 # human rnaseq 0.876
 # mouse rnaseq 0.935
 
-######
-require('data.table')
-require('tidyverse')
-require('groupdata2')
-require('glmnet')
-require('bestNormalize')
-#install.packages("~/Downloads/glmnet_3.0-2.tgz", repos=NULL)
-# **NEED GLMNET3.0**
+# ---- use the fold output to train a new model ---- #
 
-source("code/03_models/cv_utils.R")
+#save(X_train, X_test, Y_train, Y_test, 
+load(file=sprintf("data/07_model_dat/%s_%s_%s_train_mat.RData", prefix, data_type, ds))
 
-options(stringsAsFactors = FALSE)
-NFOLDS <- 6
-
-# set up the data
-
-if (data_type == "microarray"){
-  #mapping <- fread(sprintf("data/01_metadata/%s_exp_to_sample.csv", prefix), data.table=FALSE)  
-  mapping <- fread(sprintf("data/01_metadata/%s_compendia_dedup_mapping.csv", prefix, data_type), data.table = FALSE) %>%
-    rename(study_acc=study_str)
-  pos <- fread(sprintf("data/05_train_df/%s_%s_%s_pos_expr.csv",prefix, data_type, ds ), data.table=FALSE)
-  neg <- fread(sprintf("data/05_train_df/%s_%s_%s_neg_expr.csv", prefix, data_type, ds ), data.table=FALSE)
-  
+if (data_type=="microarray"){
+  standardizeFlag = TRUE
 } else {
-  MIN.READS <- 100000
-  # RNA-seq data does not have duplicates... interesting
-  mapping <- fread(sprintf("data/01_metadata/%s_exp_to_sample_counts.csv", prefix), data.table=FALSE)  %>%
-    filter(present & num_reads >= MIN.READS)
-  miceadds::load.Rdata(sprintf("data/05_train_df/%s_rnaseq_%s_pos_expr.RData", prefix, ds), "pos")
-  miceadds::load.Rdata(sprintf("data/05_train_df/%s_rnaseq_%s_neg_expr.RData", prefix, ds), "neg")
-  pos <- pos %>% rename(rid=gene_name)
-  neg <- neg %>% rename(rid=gene_name)
+  standardizeFlag = FALSE
 }
-
-
-
-pos_map <- mapping %>% filter(sample_acc %in% colnames(pos))
-neg_map <- mapping %>% filter(sample_acc %in% colnames(neg))
-my_dat <- bind_rows(pos_map %>% mutate(class=1), 
-                    neg_map %>% mutate(class=0)) %>%
-  mutate(class=factor(class), study_acc=factor(study_acc))
-
-# training + testing division
-set.seed(304)
-train_test_split <- partition_group_data(my_dat, grp_col="study_acc", nfolds=5)
-train_test_split %>% group_by(partition, class) %>% count()
-train_part <- train_test_split %>% filter(partition!=5) %>% arrange(class, sample_acc)
-test_part <- train_test_split %>% filter(partition==5) %>% arrange(class, sample_acc)
-
-Y_train <- train_part$class
-Y_test <- test_part$class
-
-# divide into folds
-fold_list <- partition_group_data(train_part %>% select(-partition), nfolds=NFOLDS) %>% arrange(class, sample_acc)
-samp_to_fold <- fold_list$partition
-names(samp_to_fold) <- fold_list$sample_acc
-
-# grab relevant dfs
-X_train <- t(cbind(neg[,train_part$sample_acc[Y_train==0]],pos[,train_part$sample_acc[Y_train==1]]))
-X_test <- t(cbind(neg[,test_part$sample_acc[Y_test==0]],pos[,test_part$sample_acc[Y_test==1]]))
-colnames(X_train) <- neg$rid
-colnames(X_test) <- neg$rid
-rownames(X_train) <- c(train_part$sample_acc)
-rownames(X_test) <- c(test_part$sample_acc)
-
-if (ds=="sex"){
-  
-  if (data_type=="rnaseq"){
-    xy_genes <- read_csv(sprintf("data/%s/%s/03_model_in/xy_genes_%s.csv", data_type,prefix, data_type))
-    xy_genes_l <- xy_genes$transcript
-  } else {
-    xy_genes <- read_csv(sprintf("data/%s/%s/04_sl_input/xy_genes.csv", data_type,prefix))
-    xy_genes_l <- xy_genes$overlap.genes
-  }
-  X_test <- X_test[,xy_genes_l]
-  X_train <- X_train[,xy_genes_l]
-}
-#####
-
-if (data_type=="rnaseq"){
-  X_train[is.na(X_train)] <- 0
-  num_zeros <- apply(X_train, 2, function(x) sum(x==0)) # remove > 70% zeros
-  X_train <- X_train[,(num_zeros/nrow(X_train) <= 0.7)]
-  
-  list_genes <- colnames(X_train)
-  X_test <- X_test[,list_genes]
-  
-  X_train <- apply(X_train, 2, function(col) boxcox(col+0.5)$x.t) 
-  X_test <- apply(X_test, 2, function(col) boxcox(col+0.5)$x.t)
-  
-}
-
-
 fit <- glmnet(X_train, Y_train, family="binomial", 
               #lambda=best.lambda, 
               alpha=best.alpha,
-              standardize=TRUE)
+              standardize=standardizeFlag)
 
 
 #preds_train <- predict(fit, newx=X_train,  type="response")
@@ -201,6 +145,7 @@ sum(preds_class_train==Y_train)/length(Y_train)
 # 0.877 rat
 # 0.878 (human RNA-seq)
 # 0.935 (mouse RNA-seq)
+# 0.777 rat microarray balanced
 preds_test <- predict(fit, newx=X_test,  s=best.lambda, type="response")
 preds_class_test <- sapply(predict(fit, newx=X_test, s=best.lambda, type="class"), as.numeric)
 sum(preds_class_test==Y_test)/length(Y_test) 
@@ -209,6 +154,7 @@ sum(preds_class_test==Y_test)/length(Y_test)
 # 0.801 rat
 # 0.882 (human RNA-seq)
 # 0.913 (mouse RNA-seq)
+# 0.735 rat microarray balanced
 
 my.lambda <- best.lambda
 # human -r: 0.07086
@@ -245,6 +191,4 @@ if (data_type=="rnaseq"){
 
 
 coef_df3 %>% write_csv(sprintf("data/07_model_dat/%s_%s_%s_coef.csv", prefix, data_type, ds))
-save(X_train, X_test, Y_train, Y_test, 
-     file=sprintf("data/07_model_dat/%s_%s_%s_train_mat.RData", prefix, data_type, ds))
 
