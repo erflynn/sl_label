99acf912c78f002b1e0ff9d98459ca24c8bfa77f
E Flynn <erflynn@stanford.edu>
2020-08-15 12:36:09 -0700

    updated metadata script

--- code/01_metadata/01_extract/03_fix_rnaseq_metadata.py
+++ code/01_metadata/01_extract/03_fix_rnaseq_metadata.py
@@ -18,32 +18,26 @@ missing_acc = set()
 list_samples = set()
 list_experiments = set()
 
-def parse_obj(my_acc, acc_type):
+def parse_obj(my_acc, acc_type, obj_f, attr_f):
 	my_f = "https://www.ebi.ac.uk/ena/data/view/%s&display=xml" %(my_acc)
 	with urllib.request.urlopen(my_f) as f:
 		tree=ET.parse(f)
 		root= tree.getroot()
 		obj = root.find(acc_type)
-		obj_dict = {}
 		if (obj is None):
-			print(my_acc)
-			missing_acc.append(my_acc)
-			return []
+			return -1
 		accession = obj.attrib["accession"]
 		if accession != my_acc:
 			print("Error mismatched accessions %s %s" % (my_acc, accession))
-			return []
+			return -1
 		title = obj.find("TITLE").text
-		obj_dict["title"]=title
+		experiments = []
 		if acc_type == "RUN":
 			exp_ref = obj.find("EXPERIMENT_REF")
 			if exp_ref is not None:
 				experiment = exp_ref.attrib["accession"]
 				list_experiments.add(experiment)
-				experiments = []
-			else: experiments = []
-		else:
-			experiments = []
+				experiments.append(experiment)
 		# find friends!
 		samples = []
 		experiments = []
@@ -62,19 +56,19 @@ def parse_obj(my_acc, acc_type):
 				list_experiments.add(link_id)
 			else:
 				t = "other link"   
-		obj_dict["experiments"]=experiments
-		obj_dict["runs"]=runs
-		obj_dict["samples"]=samples
+		obj_f.write("%s\t%s\t%s\t%s\t%s\n" %(my_acc, title, ";".join(runs), ";".join(samples), ";".join(experiments)))
 		# find attributes
-		attr_dict = {}
+		#attr_dict = {}
 		my_attr = obj.find("%s_ATTRIBUTES" %acc_type).findall("%s_ATTRIBUTE" %acc_type)
 		for attr in my_attr:
 			tag_text = attr.find("TAG").text
 			value_text = attr.find("VALUE").text
-			attr_dict[tag_text]= value_text
-		obj_dict["attr"]=attr_dict
-		return obj_dict
-	return []
+			attr_f.write("%s\t%s\t%s\n" %(my_acc, tag_text, value_text))
+			#attr_dict[tag_text]= value_text
+		#obj_dict["attr"]=attr_dict
+		#return obj_dict
+		return 1
+	return -1
 
 
 #setup
@@ -82,34 +76,28 @@ idx = sys.argv[1]
 with open("data/rnaseq_runs_%s.csv" %idx, 'r') as f:
 	list_runs = [run.rstrip("\n") for run in f]
 
+
+def parse_write(acc_type, acc_list):
+	obj_f = open("data/sra_out/%s_info_%s.tsv" %(acc_type.lower(), idx), 'w')
+	attr_f = open("data/sra_out/%s_attr_%s.tsv" %(acc_type.lower(), idx), 'w')
+	# parse the runs 
+	for my_acc in acc_list:
+		ret_val = parse_obj(my_acc, acc_type, obj_f, attr_f)
+		if ret_val==-1:
+			missing_acc.add(my_acc)
+	attr_f.close()
+	obj_f.close()
+
 start_time = time.time()
-# parse the runs 
-run_dict = {}
-for my_acc in list_runs:
-	run_dict[my_acc] = parse_obj(my_acc, "RUN")
 
-with open("data/sra_out/run_chunk_%s.json" %(idx), 'w') as f:
-	run_str = json.dumps(run_dict)
-	f.write(run_str)
-# parse the samples
-sample_dict = {}
-for my_acc in list_samples:
-	sample_dict[my_acc] = parse_obj(my_acc, "SAMPLE")
+parse_write("RUN", list_runs)
+parse_write("SAMPLE", list_samples)
+parse_write("EXPERIMENT", list_experiments)
 
-with open("data/sra_out/sample_chunk_%s.json" %(idx), 'w') as f:
-	sample_str = json.dumps(sample_dict)
-	f.write(sample_str)
-# parse the experiments
-experiment_dict = {}
-for my_acc in list_experiments:
-	experiment_dict[my_acc] = parse_obj(my_acc, "EXPERIMENT")
-
-with open("data/sra_out/experiment_chunk_%s.json" %(idx), 'w') as f:
-	experiment_str = json.dumps(experiment_dict)
-	f.write(experiment_str)
+# write out missing accessions
+with open("data/sra_out/missing_acc_%s.tsv" %(idx), 'w') as f:
+	f.write("\n".join(missing_acc))
 
 print("--- %s seconds ---" % (time.time() - start_time))
-
-
 
 
